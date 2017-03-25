027d5a23874bc01c3279bf2652ca29b5f20f51bd
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2017-03-25 22:26:28 +0530

    Code restructuring

--- MBBM_Node_API/routes/productRouter.js
+++ MBBM_Node_API/routes/productRouter.js
@@ -1,30 +1,3 @@
-"use strict";
-// BASE SETUP
-// =============================================================================
-// Mongo DB Details
-// ----------------
-// mongo ds137110.mlab.com:37110/mbbm -u <dbuser> (admin) -p <dbpassword> (password)
-// mongodb://admin:password@ds137110.mlab.com:37110/mbbm
-// ==============================================================================
-
-var express = require('express');        // Call express
-var app = express();                 // Define our app using express
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-// Connect to database
-mongoose.connect('mongodb://admin:password@ds137110.mlab.com:37110/mbbm'); 
-
-// Get Products model
-var Product = require('./models/product');
-
-// Configure bodyParser()
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // Set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // middleware to use for all requests
@@ -42,6 +15,7 @@ router.get('/', function (req, res) {
 router.route('/products')
     
     // Add a new product
+    // ============================================================
     .post(function (req, res) {
     
         var product = new Product();     
@@ -59,6 +33,7 @@ router.route('/products')
     })
 
     // Get list of all products
+    // ===========================================================
     .get(function (req, res) {
         Product.find(function (err, products) {
             if(err) {
@@ -71,6 +46,7 @@ router.route('/products')
 router.route('/products/:ProductID')
 
     // Get the product with that id
+    // ================================================================
     .get(function(req, res) {
         Product.findById(req.params.ProductID, function(err, product) {
             if (err)
@@ -80,6 +56,7 @@ router.route('/products/:ProductID')
     })
 
     // Update the product with this id
+    // ================================================================
 	.put(function(req, res) {
 		Product.findById(req.params.ProductID, function(err, product) {
 
@@ -100,6 +77,7 @@ router.route('/products/:ProductID')
 	})
 
     // Delete the product with productID
+    // ================================================================
 	.delete(function(req, res) {
 		Product.remove({
 			_id: req.params.ProductID
@@ -110,13 +88,3 @@ router.route('/products/:ProductID')
 			res.json({ message: 'Product deleted' });
 		});
 	});
-
-
-// REGISTER ROUTES -------------------------------
-// All of the routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('REST API running on port ' + port);
